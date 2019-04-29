f57204f4240acb4632f2afbe41fb6f0390caee28
vuurtio <viivi.uurtio@aalto.fi>
2019-04-29 16:03:05 +0300

    cleaning up

--- datasets/simulated-datasets/generate_trigonometric_relations.m
+++ datasets/simulated-datasets/generate_trigonometric_relations.m
@@ -10,27 +10,6 @@ function [X,Y] = generate_trigonometric_relations(n,p,q,type)
 
 % output:       X - the simulated data X
 %               Y - the simulated data Y
-
-% rng('shuffle')
-% a = -2*pi; b = 2*pi;
-% X = a + (b-a).*rand(n,p);
-% Y = a + (b-a).*rand(n,q);
-%    
-% switch type
-%     case 1 
-%         X(:,1) = sin(X(:,2)/2) + normrnd(0,0.05,[n,1]);
-%         Y(:,1) = cos(X(:,2)/3) + normrnd(0,0.05,[n,1]);
-%         Y(:,2) = cos(X(:,2)/4) + normrnd(0,0.05,[n,1]);
-%         
-%     case 2 
-%         X(:,1) = 2 * sin(X(:,2)) + normrnd(0,0.05,[n,1]);
-%         Y(:,1) = 3 * cos(X(:,2)) + normrnd(0,0.05,[n,1]);
-%         Y(:,2) = 4 * cos(X(:,2)) + normrnd(0,0.05,[n,1]);
-%         
-%     case 3 
-%         X(:,1) = 2 * sin(X(:,2)/2) + normrnd(0,0.05,[n,1]);
-%         Y(:,1) = 3 * cos(X(:,2)/3) + normrnd(0,0.05,[n,1]);
-%         Y(:,2) = 4 * cos(X(:,2)/4) + normrnd(0,0.05,[n,1]);
         
         
 rng('shuffle')
