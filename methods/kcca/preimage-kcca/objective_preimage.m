298ee918aed330ee8e4e3cad39252f31e9292f98
vuurtio <viivi.uurtio@aalto.fi>
2019-04-29 11:57:26 +0300

    edits

--- methods/kcca/preimage-kcca/objective_preimage.m
+++ methods/kcca/preimage-kcca/objective_preimage.m
@@ -8,11 +8,6 @@ switch type
     case 2 % quadratic kernel
         val = alpha' * (X * X').^2 * alpha - 2 * alpha' * (X * u).^2 + (u' * u)^2;
         
-    case 3 % cubic kernel
-        val = alpha' * (X * X').^3 * alpha - 2 * alpha' * (X * u).^3 + (u' * u)^3;
-        
-    case 4 % gaussian kernel
-        val = alpha' * rbf_kernel(X) * alpha - 2 * alpha' * rbf_kernel(X,u) + 1;
 end
 
 
